e01797843c729d12cfbf26b50e8fffe271032a1b
chadtverity <chadv@cayu.ca>
2015-03-16 10:55:26 -0600

    commit

--- script.js
+++
@@ -1,5 +0,0 @@
-var options = $("#selectVendor > option");
-
-var random = Math.floor(options.length * (Math.random() % 1));
-
-$("#selectVendor > option").attr('selected',false).eq(random).attr('selected',true);
